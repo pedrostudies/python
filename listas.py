729d962e26f39a5ab129d848c1f2fd59bbd626ca
Pedro <147367860+pedrostudies@users.noreply.github.com>
2024-03-21 16:24:37 -0300

    estudo sobre listas

--- listas.py
+++ listas.py
@@ -1,3 +1,16 @@
-numes = [2,3,4,5,6,]
+numes = [2,3,4,5,6,7] ## listas
 
-print(numes)  
+print(numes) ## printar a lista inteira
+
+print(numes[0]) ## printar apenas 1 index
+
+numes[0] = 1 ## trocar conteúdo da variável direto do index
+
+print(numes[0]) ## output de apenas um index com a váriavel trocada 
+
+print(numes[0:2]) ## slicing para output de dois ou mais valores (0 é a partir de qual index, e : quantos valores serão impressos após isso)
+
+print(len(numes)) ## retorna a quantidade de indexs
+
+print(sorted(numes, reverse=True)) ## orderna os itens em crescente ou com reverse ativo decrescente
+
