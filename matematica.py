82a6b97f99dfa57054d098af3934014d35c1a613
Pedro <147367860+pedrostudies@users.noreply.github.com>
2024-04-24 23:17:14 -0300

    funcoes math builtin e modulos

---
+++ matematica.py
@@ -0,0 +1,31 @@
+import math
+
+x = 8
+y = 100
+
+# raiz_quadrada = math.sqrt(x)
+# print(math.ceil(raiz_quadrada))
+# print(math.floor(raiz_quadrada))
+
+# logaritmo = math.log10(y)
+# print(logaritmo)
+
+print(math.pi)
+print(math.factorial(x))
+print(x / math.inf)
+
+
+
+
+
+# valores = [2,5,8,-1,0,11,7,-6]
+# print(max(valores))
+# print(min(valores))
+
+# a = -5
+# b = 4
+# print(abs(a))
+# print(pow(a, b))
+
+# c = 2.789011
+# print(round(c,1))
